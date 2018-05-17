c8bb48440dc60ad505cfdff8898379a8de0f044b
liangdong <liangdong@smzdm.com>
2018-05-17 16:32:53 +0800

    add view

---
+++ src/View.php
@@ -0,0 +1,33 @@
+<?php
+namespace myf;
+
+/**
+ * Class View
+ * @package myf
+ *
+ * 渲染模板文件
+ */
+class View
+{
+    private static $filename = '';
+    private static $params = [];
+
+    // 加载模板文件
+    private static function loadTpl()
+    {
+        extract(self::$params);
+        ob_start();
+        require self::$filename;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+    // 渲染模板
+    public static function render($tpl, $params = [])
+    {
+        self::$filename = MYF_ROOT . $tpl;
+        self::$params = $params;
+        return self::loadTpl();
+    }
+}
