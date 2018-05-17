c9a1f18ec92662c30fc909a4732889a114edfbc8
liangdong <liangdong@smzdm.com>
2018-05-17 14:04:49 +0800

    update

---
+++ src/App.php
@@ -0,0 +1,17 @@
+<?php
+namespace myf;
+
+/**
+ * Class App
+ * @package myf
+ *
+ * 加载配置, 完成路由, 唤起controller
+ */
+class App
+{
+    // 启动框架
+    public static function run($config)
+    {
+
+    }
+}
